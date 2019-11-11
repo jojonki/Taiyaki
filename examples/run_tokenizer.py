e64baaf68f0807e6848f5f25a8738e8ee0614951
jojonki <junki.ohmura@gmail.com>
2019-11-11 21:52:29 +0900

    add commonPrefixSearch example

--- examples/run_tokenizer.py
+++ examples/run_tokenizer.py
@@ -11,11 +11,22 @@ sys.path.append(parent_dir)
 from taiyaki.taiyaki import Taiyaki
 
 
+def run(taiyaki, query):
+    cp_list = taiyaki.commonPrefixSearch(query)
+    print('Common prefixes: {}'.format(cp_list))
+
+    tokens = taiyaki.tokenize(query)
+    print('Tokenized tokens (min cost):', tokens)
+
+    tokens = taiyaki.longestSearch(query)
+    print('Tokenized tokens (longest match):', tokens)
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--da_dic_file',
                         type=str,
-                        default='./models/ipadic-vocab.txt.dict',
+                        default='./data/da.dict',
                         metavar='PATH',
                         help='double-array dictionary')
     parser.add_argument('--vocab_dic_file',
@@ -38,23 +49,13 @@ def main():
     query = args.query
     if query:
         print('Input:', query)
-
-        tokens = taiyaki.tokenize(query)
-        print('Tokens (min cost):', tokens)
-
-        tokens = taiyaki.longestSearch(query)
-        print('Tokens (longest match):', tokens)
+        run(taiyaki, query)
     else:
         while True:
             query =input('Query (press "end" to exit)>> ')
             if query == 'end':
                 break
-
-            tokens = taiyaki.tokenize(query)
-            print('Tokens (min cost):', tokens)
-
-            tokens = taiyaki.longestSearch(query)
-            print('Tokens (longest match):', tokens)
+            run(taiyaki, query)
 
 
 if __name__ == '__main__':
