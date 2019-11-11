1cd912ba26b867dbe649f0d4328961b86996232f
jojonki <junki.ohmura@gmail.com>
2019-11-11 21:17:19 +0900

    improve sample

--- examples/run_tokenizer.py
+++ examples/run_tokenizer.py
@@ -13,17 +13,48 @@ from taiyaki.taiyaki import Taiyaki
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--da_dic', type=str, default='./models/ipadic-vocab.txt.dict', metavar='PATH', help='double-array dictionary')
-    parser.add_argument('--word_dic_dir', type=str, default='./data/mecab-ipadic-2.7.0-20070801/', metavar='PATH', help='')
-    parser.add_argument('--trans_def', type=str, default='./data/mecab-ipadic-2.7.0-20070801/matrix.def', metavar='PATH', help='')
-    parser.add_argument('-q', '--query', type=str, help='query')
+    parser.add_argument('--da_dic_file',
+                        type=str,
+                        default='./models/ipadic-vocab.txt.dict',
+                        metavar='PATH',
+                        help='double-array dictionary')
+    parser.add_argument('--vocab_dic_file',
+                        type=str,
+                        metavar='PATH',
+                        default='./data/vocab.dict',
+                        help='vocabulary dictionary')
+    parser.add_argument('--trans_cost_file',
+                        type=str,
+                        metavar='PATH',
+                        default='./data/trans_cost.dict',
+                        help='token transition cost dictionary')
+    parser.add_argument('-q', '--query',
+                        type=str,
+                        help='input query')
     args = parser.parse_args()
 
+    taiyaki = Taiyaki(args.da_dic_file, args.vocab_dic_file, args.trans_cost_file)
+
     query = args.query
-    taiyaki = Taiyaki(args.da_dic, args.word_dic_dir, args.trans_def)
-    tokens = taiyaki.tokenize(query)
-    print('Input:', query)
-    print('Tokens:', tokens)
+    if query:
+        print('Input:', query)
+
+        tokens = taiyaki.tokenize(query)
+        print('Tokens (min cost):', tokens)
+
+        tokens = taiyaki.longestSearch(query)
+        print('Tokens (longest match):', tokens)
+    else:
+        while True:
+            query =input('Query (press "end" to exit)>> ')
+            if query == 'end':
+                break
+
+            tokens = taiyaki.tokenize(query)
+            print('Tokens (min cost):', tokens)
+
+            tokens = taiyaki.longestSearch(query)
+            print('Tokens (longest match):', tokens)
 
 
 if __name__ == '__main__':
