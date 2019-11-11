68f95c589a12fc654a3280d3c696e54e8b4e447d
jojonki <junki.ohmura@gmail.com>
2019-11-11 21:52:41 +0900

    move double_array place

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from Cython.Distutils import build_ext
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("double_array", ["taiyaki/double_array.pyx"])]
+    ext_modules = [Extension("taiyaki.double_array", ["taiyaki/double_array.pyx"])]
 )
